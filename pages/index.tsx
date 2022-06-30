9831af376899a80f9bfc7015c1100c2205b70311
mar-cial <cesarmarcialq@gmail.com>
2022-06-30 14:30:18 -0700

    starter files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,68 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+// date created dd/mm/yy: 30 / 06 / 2022 - 11 : 24 : 14 by César Marcial
 
-const Home: NextPage = () => {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+//react / nextjs imports
+import { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next';
+import Image from 'next/image';
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+// media imports
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+// data imports
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
+// component imports
 
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
-  )
+// interfaces
+export interface ResponseData {
+  data: Datum[];
+  date_recived: DateRecived;
 }
 
-export default Home
+export interface Datum {
+  id: number;
+  property_category_id: number;
+  name: string;
+  seo_friendly: string;
+  active_record: boolean;
+  created_at: Date;
+  updated_at: Date;
+  created_by: string;
+}
+
+export interface DateRecived {
+  format: string;
+}
+
+// next data fetching
+
+// start of component
+const Homepage: NextPage = () => {
+  console.log(parents);
+
+  return (
+    <div>
+      <nav className="p-4">
+        <h1 className="text-4xl font-bold tracking-widest font-title">
+          Doorvel.
+        </h1>
+      </nav>
+
+      <main className="flex flex-col items-center justify-center px-12 py-4 md:py-8">
+        <section className="grid w-full gap-6 md:grid-cols-2">
+          <div className="flex items-center justify-center w-full">
+            <div className="w-1/2">
+              <Image
+                src={'/cat.png'}
+                width={880}
+                height={1428}
+                layout="responsive"
+              />
+            </div>
+          </div>
+
+          <div className="grid grid-rows-2 gap-4"></div>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Homepage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        title: 'Righteous',
+        text: 'Poppins',
+      },
+    },
   },
   plugins: [],
-}
+};
