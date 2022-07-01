2f54184a86a449320cbee24069a504687c3e09ae
Cesar <cesarmarcialq@gmail.com>
2022-07-01 13:57:16 -0700

    pagination container button abstrated component

--- components/PaginationContainer.tsx
+++ components/PaginationContainer.tsx
@@ -1,24 +1,18 @@
 import React, { MouseEventHandler } from 'react'
 import { motion } from 'framer-motion'
 type Props = {
-  action: MouseEventHandler<HTMLButtonElement> | undefined
+  decreaseAction: MouseEventHandler<HTMLButtonElement> | undefined
+  increaseAction: MouseEventHandler<HTMLButtonElement> | undefined
+
   page: number | undefined
 }
 
 const PaginationContainer = (props: Props) => {
   return (
     <motion.div className="flex items-center justify-between px-6 py-4">
-      <button onClick={() => setPage((prev) => (prev > 1 ? prev - 1 : prev))}>
-        Decrease
-      </button>
-      <p>{page}</p>
-      <button
-        onClick={() =>
-          setPage((prev) => (prev < data.count / 100 ? prev + 1 : prev))
-        }
-      >
-        Increase
-      </button>
+      <button onClick={props.decreaseAction}>Decrease</button>
+      <p>{props.page}</p>
+      <button onClick={props.increaseAction}>Increase</button>
     </motion.div>
   )
 }
