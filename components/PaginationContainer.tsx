0c035742110ac8d8d669dbeababbb9eae5cfcad8
Cesar <cesarmarcialq@gmail.com>
2022-07-01 14:27:11 -0700

    styled buttons

--- components/PaginationContainer.tsx
+++ components/PaginationContainer.tsx
@@ -10,9 +10,23 @@ type Props = {
 const PaginationContainer = (props: Props) => {
   return (
     <motion.div className="flex items-center justify-between px-6 py-4">
-      <button onClick={props.decreaseAction}>Decrease</button>
-      <p>{props.page}</p>
-      <button onClick={props.increaseAction}>Increase</button>
+      <motion.button
+        onClick={props.decreaseAction}
+        className="border-2 border-black p-1 md:px-3 md:py-2 text-sm md:text-lg"
+        whileHover={{ y: -2 }}
+        whileTap={{ y: 0 }}
+      >
+        Decrease
+      </motion.button>
+      <p className="font-title text-2xl md:text-3xl">{`Page: ${props.page}`}</p>
+      <motion.button
+        onClick={props.increaseAction}
+        className="border-2 border-black p-1 md:px-3 md:py-2 text-sm md:text-lg"
+        whileHover={{ y: -2 }}
+        whileTap={{ y: 0 }}
+      >
+        Increase
+      </motion.button>
     </motion.div>
   )
 }
