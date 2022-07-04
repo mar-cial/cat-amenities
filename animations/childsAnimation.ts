183be9f35c377be11f33bc24d65ca1e27d25fa07
mar-cial <cesarmarcialq@gmail.com>
2022-07-04 09:59:45 -0700

    animations for the childs main container

--- animations/childsAnimation.ts
+++ animations/childsAnimation.ts
@@ -1,27 +1,15 @@
-import { Variants } from "framer-motion"
+import { Variants } from 'framer-motion';
 
 export const mainContentVariants: Variants = {
-    enter: {
-        opacity: 1,
-        y: 0
-    },
-    initial: {
-        opacity: 0
-    },
-    exit: {
-        opacity: 0,
-        y: -2
-    }
-}
-
-export const loadingVariants: Variants = {
-    enter: {
-        height: 50
-    },
-    initial: {
-        height: 0
-    },
-    exit: {
-        height: 0
-    }
-}
+  enter: {
+    opacity: 1,
+    y: 0,
+  },
+  initial: {
+    opacity: 0,
+  },
+  exit: {
+    opacity: 0,
+    y: -2,
+  },
+};
