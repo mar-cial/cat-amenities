7c400f73f54eed4703e52e42baaa5de32cb2d66a
mar-cial <cesarmarcialq@gmail.com>
2022-07-04 10:21:44 -0700

    the homepage header

---
+++ components/HomepageHeaderView.tsx
@@ -0,0 +1,13 @@
+const HomepageHeaderView = () => {
+  return (
+    <header className="flex flex-col gap-6 py-4 text-center">
+      <h2 className="text-4xl font-bold ">Welcome to Doorvel.</h2>
+      <div className="px-6 md:px-0">
+        <p>We have the best selection of stuff available.</p>
+        <p>Get 'em while they're hot!</p>
+      </div>
+    </header>
+  );
+};
+
+export default HomepageHeaderView;
